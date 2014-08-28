f465231f274a0199ab0510f10a2f3847aea83b26
Wahyu Taufik <wahyutaufik37@gmail.com>
2014-08-28 10:42:20 +0700

    First commit

---
+++ templates/layout.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Mahabharat Download and Streaming</title>
+	<link rel="stylesheet" type="text/css" href="<?php echo Theme::base('vendor/css/naked.min.css') ?>">
+	<link rel="stylesheet" type="text/css" href="<?php echo Theme::base('vendor/css/style.css') ?>">
+</head>
+<body>
+	<div>
+		<nav class="navbar-menu">
+		   <div class="wrapper">
+		   	<img style="width:195px; margin-left:20px;>" src="<?php echo URL::base('vendor/img/mahabharat-logo.jpg') ?>">
+		      <a href="#" class="brand"><?php echo f('page.title', 'Bono Application') ?></a>
+		      <section class="topbar pull-right">
+		         <!-- <ul class="menu">
+		            <li class="active"><a href="#">Menu 1</a></li>
+		            <li class="divide"></li>
+		            <li><a href="#">Menu 2</a></li>
+		            <li class="divide"></li>
+		            <li class="collapsible">
+		               <a href="#">Menu 3</a>
+		               <ul>
+		                  <li><a href="#">Web Design</a></li>
+		                  <li><a href="#">Web Development</a></li>
+		                  <li class="divider"></li>
+		                  <li><a href="#">Illustrations</a></li>
+		               </ul>
+		            </li>
+		         </ul> -->
+		      </section>
+		   </div>
+		</nav>
+	</div>
+	<div class="tabstrip">
+	   <div class="row">
+	      <a class="tabspan-2 tabs" href="#">
+	         <!-- <span class="icon star"></span> -->
+	         <span>Featured</span>
+	      </a>
+	      <a class="tabspan-2 tabs" href="#">
+	         <!-- <span class="icon profile"></span> -->
+	         <span>Profile</span>
+	      </a>
+	      <a class="tabspan-2 tabs" href="#">
+	         <!-- <span class="icon search"></span> -->
+	         <span>Search</span>
+	      </a>
+	      <a class="tabspan-2 tabs" href="#">
+	         <!-- <span class="icon more"></span> -->
+	         <span>More</span>
+	      </a>
+	      <a class="tabspan-2 tabs" href="#">
+	         <!-- <span class="icon more"></span> -->
+	         <span>More</span>
+	      </a>
+	   </div>
+	</div>
+	
+</body>
+</html>
